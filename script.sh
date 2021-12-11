3b22b574046bb237a3ac25a8abf9570ec9c315dd
Firibu Anyass <42239000+Fas96@users.noreply.github.com>
2021-12-12 00:43:13 +0900

    Create script.sh
    "testing git script actions"

---
+++ script.sh
@@ -0,0 +1,15 @@
+  echo what is in this directory ?
+  java -version
+  echo
+  echo 3. Is hit installed?
+  git --version
+  echo 4. what is teh android sdk root
+  echo $ANDROID_SDK_ROOT
+  echo
+  echo 5. Wher is the android sdk root?
+  echo 6. who is runnning this script?
+  whoami
+  echo 7. how is the disc laid out 
+  df
+  echo 10. what environment variables are available?
+  env
